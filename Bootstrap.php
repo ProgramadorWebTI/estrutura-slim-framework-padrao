18f6f599b9c1aca0513a6bb9d4fdbbf63b13bbe5
ProgramadorWebTI <programadorwebti@gmail.com>
2018-08-19 23:54:11 -0300

    Adicionando function base_url na constante, URL_BASE e ASSETS

--- Bootstrap.php
+++ Bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
-define("URL_BASE", "http://192.168.1.100/projetoSlimPadrao/");
-define("ASSETS", "http://192.168.1.100/projetoSlimPadrao/public/assets/");
+define("URL_BASE", base_url());
+define("ASSETS", base_url() . "public/assets/");
 define("DIRETORIO_BASE", $_SERVER['DOCUMENT_ROOT']);
 function base_url($args = null)
 {
